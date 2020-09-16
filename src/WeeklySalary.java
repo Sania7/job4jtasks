d43e146b32a895270cbc218c036b732d6909533c
macbook <79824177721@yandex.ru>
2020-09-16 23:20:58 +0500

    Одинаковые элементы для массивов

---
+++ src/WeeklySalary.java
@@ -0,0 +1,17 @@
+public class WeeklySalary {
+    public static int calculate(int[] hours) {
+        int salary = 0;
+        int rate = 10;
+        int overNorm = 15;
+        int workOnTheDayOff = 20;
+        int aboveTheNormOnTheDayOff = 30;
+        int[] week = {1, 2, 3, 4, 5, 6, 7};
+        for (int i = 0; i < week.length; i++) {
+            if (week[i] != 6 || week[i] != 7) {
+//                salary = rate * hours;
+            }
+        }
+
+        return salary;
+    }
+}
