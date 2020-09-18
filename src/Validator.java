54f70b7419ca1e9878a607b8cf12494513be2aef
macbook <79824177721@yandex.ru>
2020-09-18 14:19:41 +0500

    Проверка массива на уникальность элементов.

---
+++ src/Validator.java
@@ -0,0 +1,15 @@
+public class Validator {
+    public static boolean checkArray(int[] data, int value) {
+        boolean rsl = true;
+        int count = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == value) {
+                count++;
+                if (count >= data.length / 2) {
+                    return false;
+                }
+            }
+        }
+        return rsl;
+    }
+}
