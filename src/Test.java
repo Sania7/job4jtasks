778a2c874a719c428a98fe34fd72bc928cf1ea1c
macbook <79824177721@yandex.ru>
2020-09-22 13:58:10 +0500

    Заполнить треугольную матрицу

--- src/Test.java
+++ src/Test.java
@@ -1,24 +1,14 @@
 public class Test {
-    public static int indexOf(char[] string, char c, int number) {
-        int rsl = -1;
-        int count = 0;
-        for (int i = 0; i < string.length; i++) {
-            if (string[i] == c) {
-                count++;
-                if (count == number) {
-                    return i;
-                }
-            }
-        }
-        return rsl;
-    }
     public static void main(String[] args) {
-        char[] string = new char[] {'b', 'a', 'y', 'o', 'f'};
-        for (int i = 0; i < string.length; i++) {
-
-
-
+        int[][] list = new int[10][];
+        int count = 1;
+        for (int i = 0; i < 10; i++) {
+            list[i] = new int[i + 1];
+            for (int j = 0; j < 10; j++) {
+                count++;
+                System.out.print(list[i][j]);
+            }
+            System.out.println();
         }
     }
 }
-
