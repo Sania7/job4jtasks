15f53d6345f7eb455dc1402ee8751a5ddbf158a9
macbook <79824177721@yandex.ru>
2020-09-23 15:39:00 +0500

    5. Индексы в массивах

---
+++ src/ArrayIndex.java
@@ -0,0 +1,11 @@
+public class ArrayIndex {
+    public static int getFirstElement(int[] array) {
+        int rsl = array[0];
+        return rsl;
+    }
+
+    public static int getLastElement(int[] array) {
+        int rsl = array[array.length - 1];
+        return rsl;
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,10 @@
 public class Test {
-    public static void main(String[] args) {
+    public static int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int leftPoint = 0;
+        int rightPoint = 0;
+        int rslPoint = 0;
 
+        return rsl;
     }
 }
