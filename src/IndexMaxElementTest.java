3d3397ecccb73d2ff1f721fd428607efa62fb186
macbook <79824177721@yandex.ru>
2020-09-23 15:27:07 +0500

    8. Индекс максимального значения

---
+++ src/IndexMaxElementTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class IndexMaxElementTest {
+
+    @Test
+    public void getIndexMax() {
+        int[] in = {1, 2, 3, 4, 5};
+        int expected = 4;
+        int rsl = IndexMaxElement.getIndexMax(in);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void getIndexMaxIs0() {
+        int[] in = {5, 4, 3, 2, 1};
+        int expected = 0;
+        int rsl = IndexMaxElement.getIndexMax(in);
+        assertThat(rsl, is(expected));
+    }
+}
