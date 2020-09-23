ec66e1c8cd4ea133770ec784f418d84e58d2e0d6
macbook <79824177721@yandex.ru>
2020-09-23 12:43:31 +0500

    3. Объединить два отсортированных массива

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        int[][] list = new int[10][];
-        int count = 1;
-        for (int i = 0; i < 10; i++) {
-            list[i] = new int[i + 1];
-            for (int j = 0; j < 10; j++) {
-                count++;
-                System.out.print(list[i][j]);
-            }
-            System.out.println();
-        }
+
     }
 }
