62bd1ae9480c4f4bae35480b395c8935150c344e
macbook <79824177721@yandex.ru>
2020-09-25 13:57:11 +0500

    Транспонировать матрицу

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,19 @@
 public class Test {
-    public static int[] merge(int[] left, int[] right) {
-        int[] rsl = new int[left.length + right.length];
-        int leftPoint = 0;
-        int rightPoint = 0;
-        int rslPoint = 0;
-
-        return rsl;
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] a = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = n * i + j;
+            }
+        }
+        System.out.println("Начальная матрица");
+        System.out.println("-------");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[i][j] + "    ");
+            }
+            System.out.println();
+        }
     }
 }
