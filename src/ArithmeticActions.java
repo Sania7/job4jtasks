b7c78b3363f4e0e9c9ecc84dd17a0aab69bbb280
macbook <79824177721@yandex.ru>
2020-09-29 13:02:21 +0500

    Арифметические операции.

---
+++ src/ArithmeticActions.java
@@ -0,0 +1,18 @@
+public class ArithmeticActions {
+    public  static String selectAction(int left, int right, int rsl) {
+        if (left + right == rsl) {
+            return "added";
+        }
+        if (left * right == rsl) {
+            return "multiplied";
+        }
+        if (left / right == rsl) {
+            return "divided";
+        }
+        if (left - right == rsl) {
+            return "subtracted";
+        } else {
+            return "none";
+        }
+    }
+}
