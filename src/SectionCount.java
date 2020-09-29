d01552c82850c34d91e327993b27d1d27d8ab73e
macbook <79824177721@yandex.ru>
2020-09-29 14:21:30 +0500

    Найти количество отрезков, используя цикл

---
+++ src/SectionCount.java
@@ -0,0 +1,10 @@
+public class SectionCount {
+    public static int count(int length, int section) {
+        int rsl = 0;
+        while (length >= section) {
+            rsl = length / section;
+            return rsl;
+        }
+       return rsl;
+    }
+}
