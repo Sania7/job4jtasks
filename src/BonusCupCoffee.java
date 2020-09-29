66bc4a96d02785e8a9a34347cfc7f8dfc9491dd0
macbook <79824177721@yandex.ru>
2020-09-29 11:55:51 +0500

    Бонусные чашки кофе.

--- src/BonusCupCoffee.java
+++ src/BonusCupCoffee.java
@@ -1,8 +1,5 @@
 public class BonusCupCoffee {
     public static int countCup(int count, int n) {
-        int amount = count / n;
-        int sum = count + amount;
-
-        return sum;
+        return  count + (count / n);
     }
 }
