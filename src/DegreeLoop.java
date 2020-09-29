4601c3b65563203a1e9dd9bf005021835f8885de
macbook <79824177721@yandex.ru>
2020-09-29 14:02:44 +0500

    Найти степень числа

---
+++ src/DegreeLoop.java
@@ -0,0 +1,9 @@
+public class DegreeLoop {
+    public static int calculate(int a, int b) {
+        int rsl = 1;
+        for (int i = 1; i <= b; i++) {
+            rsl = rsl * a;
+        }
+        return rsl;
+    }
+}
