718894de19675742e72a6654cbbe29aa290d9b36
macbook <79824177721@yandex.ru>
2020-11-06 13:47:09 +0500

    40. Инициалы

---
+++ src/InitialsTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InitialsTest {
+
+    @Test
+    public void convert() {
+        assertEquals("Иванов И.И.", Initials.convert(new String[] {"Иванов", "Иван", "Иваныч"}));
+        assertEquals("Григорьев И.Н.", Initials.convert(new String[] {"Григорьев", "Игорь", "Николаевич"}));
+    }
+}
--- src/test/Test1.java
+++ src/test/Test1.java
@@ -1,8 +1,28 @@
 package test;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Test1 {
     public static void main(String[] args) {
-        Integer x = 5;
-        System.out.println(x.toString());
+        System.out.println("Введиде ФИО: ");
+        Scanner scanner = new Scanner(System.in);
+        String fio = scanner.nextLine();
+        Pattern pattern = Pattern.compile("[А-ЯЁ][а-яё]+\\s[А-ЯЁ][а-яё]+\\s?-?[А-ЯЁ][а-яё]+-?([А-ЯЁ]?[а-яё])+");
+        Matcher matcher = pattern.matcher(fio);
+        if (matcher.matches()) {
+            String[]part = fio.split("\\s+");
+            String[]FIO = {"Фамилия: ", "Имя: ","Отчество: "};
+
+            for (int i = 0;i < part.length;i++) {
+                System.out.println(FIO[i] + " " + part[i]);}
+        }
+        else
+        {
+            System.out.println("Введите Фамилию Имя Отчество!");
+        }
     }
 }
+
