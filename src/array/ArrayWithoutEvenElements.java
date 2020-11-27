489bc66985f23d8e1d108aaa7673cf44bd59c7a0
macbook <79824177721@yandex.ru>
2020-11-27 08:45:50 +0500

    59. Раздвинуть элементы массива

---
+++ src/array/ArrayWithoutEvenElements.java
@@ -0,0 +1,26 @@
+package array;
+
+/*
+37. Массив без четных значений.
+Метод принимает массив целочисленных значений.
+Необходимо перебрать этот массив выбрать из него только нечетные числа и вернуть новый массив,
+ в котором будут только нечетные числа.
+ При этом длина нового массива должна совпадать с количеством нечетных значений в исходном массиве.
+В этой задаче будет необходимо использовать оператор continue.
+ */
+
+public class ArrayWithoutEvenElements {
+    public static int[] changeData(int[] data) {
+        int[] out = new int[data.length / 2];
+        int index = 0;
+        while (index < out.length) {
+            for (int datum : data) {
+                if (datum % 2 != 0) {
+                    out[index] = datum;
+                    index++;
+                }
+            }
+        }
+         return out;
+    }
+}
