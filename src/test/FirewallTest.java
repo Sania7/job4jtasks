3a9d31cb70e8219454d023d1a5ddbea195f6c107
Alexandr Bannikov <79824177721@yandex.ru>
2021-08-07 12:47:49 +0900

    0.4. Собственный файерволл

---
+++ src/test/FirewallTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.junit.Test;
+import sethashset.Firewall;
+import static org.hamcrest.core.Is.is;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class FirewallTest {
+    @Test
+    public void checkName() {
+        Set<String> words = Set.of("instagram", "фото", "мем", "котик");
+        String s = "Создание многомодульного Gradle проекта SpringBoot + Angular в IDEA";
+        String rsl = Firewall.checkName(s, words);
+        assertThat(rsl, is("Вы сделали правильный выбор!"));
+    }
+
+    @Test
+    public void checkNameFalse() {
+        Set<String> words = Set.of("instagram", "фото", "мем", "котик");
+        String s = "Криштиану Роналду опубликовал новую фотографию с детьми в instagram";
+        String rsl = Firewall.checkName(s, words);
+        assertThat(rsl, is("Выберите другую статью..."));
+    }
+}
